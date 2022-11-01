3fdc7b0355a077ea11ade903f8c2db083c38e9c2
SP <stephenparkhum@outlook.com>
2022-10-31 17:07:33 -0700

    Adding linkoverlay

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Image, Badge } from '@chakra-ui/react'
+import { LinkBox, Box, Image, Badge, LinkOverlay } from '@chakra-ui/react'
 import { StarIcon } from '@chakra-ui/icons'
 // import React from 'react'
 import { House } from '../types/types'
@@ -14,61 +14,62 @@ const HouseCard = ({ house }: Props) => {
 
   return (
     <>
-      <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-
-        <Box p='6'>
-          <Box display='flex' alignItems='baseline'>
-            <Badge borderRadius='full' px='2' colorScheme={!!isAirbnb ? 'red' : 'yellow'}>
-              {!!isAirbnb ? 'Airbnb' : 'Residence'}
-            </Badge>
-            <Box
-              color='gray.500'
-              fontWeight='semibold'
-              letterSpacing='wide'
-              fontSize='xs'
-              textTransform='uppercase'
-              ml='2'
-            >
-              {beds} beds &bull; {house.baths} baths
+      <LinkBox maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+        <LinkOverlay href="/house">
+          <Box p='6'>
+            <Box display='flex' alignItems='baseline'>
+              <Badge borderRadius='full' px='2' colorScheme={!!isAirbnb ? 'red' : 'yellow'}>
+                {!!isAirbnb ? 'Airbnb' : 'Residence'}
+              </Badge>
+              <Box
+                color='gray.500'
+                fontWeight='semibold'
+                letterSpacing='wide'
+                fontSize='xs'
+                textTransform='uppercase'
+                ml='2'
+              >
+                {beds} beds &bull; {house.baths} baths
+              </Box>
             </Box>
+
+            <Box
+              mt='1'
+              fontWeight='semibold'
+              as='h4'
+              lineHeight='tight'
+              noOfLines={1}
+            >
+              {house.name}
+            </Box>
+            {isAirbnb && (
+              <>
+                <Box>
+                  ${house.airbnbPrice}
+                  <Box as='span' color='gray.600' fontSize='sm'>
+                    / night
+                  </Box>
+                </Box>
+
+                <Box display='flex' mt='2' alignItems='center'>
+                  {Array(5)
+                    .fill('')
+                    .map((_, i) => (
+                      <StarIcon
+                        key={i}
+                        color={i < house.baths ? 'teal.500' : 'gray.300'}
+                      />
+                    ))}
+                  <Box as='span' ml='2' color='gray.600' fontSize='sm'>
+                    {house.baths} reviews
+                  </Box>
+                </Box>
+              </>
+            )}
           </Box>
 
-          <Box
-            mt='1'
-            fontWeight='semibold'
-            as='h4'
-            lineHeight='tight'
-            noOfLines={1}
-          >
-            {house.name}
-          </Box>
-
-          {isAirbnb && (
-            <>
-              <Box>
-                ${house.airbnbPrice}
-                <Box as='span' color='gray.600' fontSize='sm'>
-                  / night
-                </Box>
-              </Box>
-
-              <Box display='flex' mt='2' alignItems='center'>
-                {Array(5)
-                  .fill('')
-                  .map((_, i) => (
-                    <StarIcon
-                      key={i}
-                      color={i < house.baths ? 'teal.500' : 'gray.300'}
-                    />
-                  ))}
-                <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-                  {house.baths} reviews
-                </Box>
-              </Box>
-            </>
-          )}
-        </Box>
-      </Box>
+        </LinkOverlay>
+      </LinkBox>
     </>
   )
 }
